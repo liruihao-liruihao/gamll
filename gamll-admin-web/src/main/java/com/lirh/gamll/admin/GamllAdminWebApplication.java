f65986f07d656a44a9ba9ae78d2347419dc8592d
lirh-set <1647548586@qq.com>
2020-03-03 22:29:33 +0800

    项目结构创建完成。。。

---
+++ gamll-admin-web/src/main/java/com/lirh/gamll/admin/GamllAdminWebApplication.java
@@ -0,0 +1,13 @@
+package com.lirh.gamll.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamllAdminWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamllAdminWebApplication.class, args);
+    }
+
+}
---
+++ gamll-pms/src/main/java/com/lirh/gamll/pms/GamllPmsApplication.java
@@ -0,0 +1,13 @@
+package com.lirh.gamll.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamllPmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamllPmsApplication.class, args);
+    }
+
+}
